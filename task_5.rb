585abd61e60fdd2522d3d79173a6871d1b01cc64
SaurabhJ-webonise <saurabh.j@weboniselab.com>
2023-02-24 17:42:14 +0530

    feature string_2 | find position of word in string

--- task_5.rb
+++ task_5.rb
@@ -1,10 +1,22 @@
 # frozen_string_literal: true
 
+string = 'RUBY parses a file by looking for one of the special tags that tells it to start interpreting the text as RUBY code. The parser then executes all of the code it finds until it runs into a RUBY closing tag.'
+word = 'RUBY'
+
 # finds the occurances of word in string
 def find_occurance(str, word)
   str.split(/ /).count(word)
 end
 
-string = 'RUBY parses a file by looking for one of the special tags that tells it to start interpreting the text as RUBY code. The parser then executes all of the code it finds until it runs into a RUBY closing tag.'
-word = 'RUBY'
 puts find_occurance string, word
+
+# find the position of word 'RUBY'
+def find_position(str, word)
+  pos = -1
+  until pos.nil?
+    pos = str.index(word, pos + 1)
+    puts pos
+  end
+end
+
+find_position string, word
