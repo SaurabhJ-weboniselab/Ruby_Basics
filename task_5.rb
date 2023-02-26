6ff8ec0f0aa4b4b48d0db171c4faf6d009b414b6
SaurabhJ-webonise <saurabh.j@weboniselab.com>
2023-02-26 15:56:16 +0530

    feature-string | capitalize the string

--- task_5.rb
+++ task_5.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 string = 'RUBY parses a file by looking for <br/> one of the special tags that tells it to start interpreting the text as RUBY code. The parser then executes all of the code it finds until it runs into a RUBY closing <br/> tag.'
+string2 = "RUBY does not require (or support) explicit type definition in variable declaration; a variable's type is determined by the context in which the variable is used."
 word = 'RUBY'
 
 # finds the occurances of word in string
@@ -8,7 +9,7 @@ def find_occurance(str, word)
   str.split(/ /).count(word)
 end
 
-puts find_occurance string, word
+# puts find_occurance string, word
 
 # find the position of word 'RUBY'
 def find_position(str, word)
@@ -19,7 +20,7 @@ def find_position(str, word)
   end
 end
 
-find_position string, word
+# find_position string, word
 
 # prints element in reverse from the array
 def reverse_string_array(str)
@@ -29,4 +30,6 @@ def reverse_string_array(str)
   reverse_string_array(str)
 end
 
-puts reverse_string_array string.split(/ /)
+# puts reverse_string_array string.split(/ /)
+
+puts string.capitalize
